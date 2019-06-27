4841ebfaa349be76540b294a45503f29a9c17948
aditilham <aditilham152@gmail.com>
2019-06-27 15:27:10 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+"use strict"
+
+let pi = 3.141592653589793
+
+//showing pi digit
+console.log(`pi (π) = ${pi}`)
+alert(`pi (π) = ${pi}`)
+
+//fixing pi to useable pi
+let piAfterFixed = (pi.toFixed(2)) // 3.14
+
+console.log(`pi after fixed will be change to ${piAfterFixed}`)
+alert(`pi after fixed will be change to ${piAfterFixed}`)
